17aeb0c09a86cf214ddd6fa82367689f57108877
Alex F <Firthir@users.noreply.github.com>
2017-12-15 15:07:38 +1000

    SEO verifications

--- header.php
+++ header.php
@@ -6,6 +6,10 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS" href="<?php bloginfo('rss2_url'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0" />
+	
+	<meta name="google-site-verification" content="_dvJbwcrL6miDaxPd3oYM1hH7Vw38oStzKxFatxx2sQ" />
+	<meta name="msvalidate.01" content="B0112812CA622A13C327AF572E2BA5B5" />
+	
 	<?php wp_head(); ?>
 </head>
 
